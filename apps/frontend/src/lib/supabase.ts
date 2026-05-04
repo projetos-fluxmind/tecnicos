9b2d33f4dd39d4d0b8f8de40194399b7129c59de
projetos-fluxmind <uendelcharles77@gmail.com>
2026-05-03 23:59:20 -0300

    fix: removendo o throw error do supabase completamente

--- apps/frontend/src/lib/supabase.ts
+++ apps/frontend/src/lib/supabase.ts
@@ -10,10 +10,6 @@ export function getSupabaseBrowserClient() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://yxgynsajxtnqhjqlhhpt.supabase.co";
   const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_BbuinIK2E3WpFeCjqgNynw_QHWNS0dU";
 
-  if (!supabaseUrl || !supabasePublishableKey) {
-    throw new Error("Configure NEXT_PUBLIC_SUPABASE_URL e NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY.");
-  }
-
   browserClient = createClient<any>(supabaseUrl, supabasePublishableKey);
   return browserClient;
 }
